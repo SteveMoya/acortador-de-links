d9550cff6d60c4ab0616a53c99354138dcf27cef
Steve Moya Cepeda <100615052@est.uasd.edu.do>
2024-05-02 19:23:15 -0400

    feat: Add User and ShortenedUrl tables to database configuration

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,26 @@
-import { defineDb } from 'astro:db';
+import { column, defineDb, defineTable } from 'astro:db';
+
+const User = defineTable({
+  columns: {
+    id: column.number({primaryKey: true, unique: true, autoIncrement: true}),
+    email: column.text(),
+    name: column.text(),
+  }
+})
+
+const ShortenedUrl = defineTable({
+  columns: {
+    userID: column.number({references: () => User.columns.id}),
+    url: column.text(),
+    shortUrl: column.text(),
+  }
+
+})
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: {
+    User,
+    ShortenedUrl,
+  }
 });
