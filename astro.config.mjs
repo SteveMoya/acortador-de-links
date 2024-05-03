66af1d645be4fd5c3282d9882c5dc7135830b0cf
Steve Moya Cepeda <100615052@est.uasd.edu.do>
2024-05-03 01:59:34 -0400

    chore: Update tsconfig, tailwind.config, astro.config, and package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
 import auth from "auth-astro";
-
 import tailwind from "@astrojs/tailwind";
 import db from "@astrojs/db";
 import icon from "astro-icon";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [auth(), tailwind(), db(), icon()]
+  integrations: [auth(), tailwind(), db(), icon(), react()]
 });
